215cebae98934f0ea8e9dc05f1ce09b636c25120
suhel khan <suhelkhan87514@gmail.com>
2023-01-11 21:15:30 +0530

    added a code

---
+++ pattern.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+    int i, j, N;
+
+    printf("Enter N: ");
+    scanf("%d", &N);
+
+    for (i = 1; i <= N; i++)
+    {
+        for (j = 1; j <= i; j++)
+        {
+            printf("%d", j);
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
