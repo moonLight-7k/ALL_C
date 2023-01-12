0dcf711efe7f00ad6d63867dee799c87b6546aaf
suhel khan <suhelkhan87514@gmail.com>
2023-01-13 00:32:12 +0530

    added the code

--- pattern1.c
+++ pattern1.c
@@ -7,4 +7,13 @@ int main(void)
 
     printf("Entre a number:");
     scanf("%d", &input);
+
+    for (int i = 1; i <= input; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
 }
