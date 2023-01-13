af3dc858f59d700ad95823e7e729f61511ce605a
suhel khan <suhelkhan87514@gmail.com>
2023-01-14 00:28:15 +0530

    some change

--- addDigonal.c
+++ addDigonal.c
@@ -1,49 +1,47 @@
 
-    #include <stdio.h>
+#include <stdio.h>
 
-    void main ()
+void main()
+{
+
+    static int array[10][10];
+    int i, j, m, n, a = 0, sum = 0;
+
+    printf("Enetr the order of the matix \n");
+    scanf("%d %d", &m, &n);
+
+    if (m == n)
     {
- 
-        static int array[10][10];
-        int i, j, m, n, a = 0, sum = 0;
- 
-        printf("Enetr the order of the matix \n");
-        scanf("%d %d", &m, &n);
- 
-        if (m == n ) 
+
+        printf("Enter the co-efficients of the matrix\n");
+        for (i = 0; i < m; ++i)
         {
- 
-            printf("Enter the co-efficients of the matrix\n");
-            for (i = 0; i < m; ++i)
+            for (j = 0; j < n; ++j)
             {
-                for (j = 0; j < n; ++j)
-                {
-                    scanf("%d", &array[i][j]);
-                }
+                scanf("%d", &array[i][j]);
             }
- 
-            printf("The given matrix is \n");
-            for (i = 0; i < m; ++i) 
-            {
-                for (j = 0; j < n; ++j)
-                {
-                    printf(" %d", array[i][j]);
-                }
-                printf("\n");
-            }
- 
-            for (i = 0; i < m; ++i) 
-            {
-                sum = sum + array[i][i];
-                a = a + array[i][m - i - 1];
-            }
- 
-            printf("\nThe sum of the main diagonal elements is = %d\n", sum);
-            printf("The sum of the off diagonal elements is   = %d\n", a);
- 
         }
- 
-        else
-            printf("The given order is not square matrix\n");
- 
+
+        printf("The given matrix is \n");
+        for (i = 0; i < m; ++i)
+        {
+            for (j = 0; j < n; ++j)
+            {
+                printf(" %d", array[i][j]);
+            }
+            printf("\n");
+        }
+
+        for (i = 0; i < m; ++i)
+        {
+            sum = sum + array[i][i];
+            a = a + array[i][m - i - 1];
+        }
+
+        printf("\nThe sum of the main diagonal elements is = %d\n", sum);
+        printf("The sum of the off diagonal elements is   = %d\n", a);
     }
+
+    else
+        printf("The given order is not square matrix\n");
+}
--- transpose.c
+++ transpose.c
@@ -1,43 +1,37 @@
 #include <stdio.h>
 
-#define N 3 // Number of rows and columns in the matrix
-
-// Find the transpose of a matrix and return the result
-int **transpose(int a[N][N])
-{
-    int **result = (int **)malloc(N * sizeof(int *));
-    for (int i = 0; i < N; i++)
-    {
-        result[i] = (int *)malloc(N * sizeof(int));
-    }
-
-    for (int i = 0; i < N; i++)
-    {
-        for (int j = 0; j < N; j++)
-        {
-            result[i][j] = a[j][i];
-        }
-    }
-    return result;
-}
-
 int main()
 {
-    // Initialize a matrix
-    int a[N][N] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+    int r, c;
 
-    // Find the transpose of the matrix
-    int **result = transpose(a);
+    printf("No. of rows:");
+    scanf("%d", &r);
 
-    // Print the result
-    for (int i = 0; i < N; i++)
+    printf("No. of columns:");
+    scanf("%d", &c);
+
+    printf("Enter the elements\n");
+
+    int arr[r][c];
+
+    for (int i = 0; i < r; i++)
     {
-        for (int j = 0; j < N; j++)
+        for (int j = 0; j < c; j++)
         {
-            printf("%d ", result[i][j]);
+            scanf("%d", &arr[i][j]);
+        }
+    }
+    printf("\n");
+
+    printf("Transpose of matrix is:\n");
+
+    for (int i = 0; i < r; i++)
+    {
+        for (int j = 0; j < c; j++)
+        {
+            printf("%d ", arr[j][i]);
         }
         printf("\n");
     }
-
     return 0;
 }
